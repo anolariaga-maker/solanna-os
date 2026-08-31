8c07acbdbe62f7eaa35cc57211a36cbd26f27154
anolariaga-maker <anolariaga@gmail.com>
2026-08-31 20:19:28 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,65 @@
-console.log("SolannaOS iniciado");
+function mostrarModulo(modulo) {
+
+    const contenido = document.getElementById("contenido");
+
+    if (modulo === "ventas") {
+
+        contenido.innerHTML = `
+            <div class="card">
+
+                <h2>Nueva Venta</h2>
+
+                <input type="text" placeholder="Cliente">
+
+                <input type="text" placeholder="Producto">
+
+                <input type="number" placeholder="Cantidad">
+
+                <input type="number" placeholder="Precio">
+
+                <button>Guardar Venta</button>
+
+            </div>
+        `;
+    }
+
+    if (modulo === "compras") {
+
+        contenido.innerHTML = `
+            <div class="card">
+                <h2>Nueva Compra</h2>
+                <p>Módulo en construcción.</p>
+            </div>
+        `;
+    }
+
+    if (modulo === "clientes") {
+
+        contenido.innerHTML = `
+            <div class="card">
+                <h2>Clientes</h2>
+                <p>Módulo en construcción.</p>
+            </div>
+        `;
+    }
+
+    if (modulo === "productos") {
+
+        contenido.innerHTML = `
+            <div class="card">
+                <h2>Productos</h2>
+                <p>Módulo en construcción.</p>
+            </div>
+        `;
+    }
+
+    if (modulo === "caja") {
+
+        contenido.innerHTML = `
+            <div class="card">
+                <h2>Caja</h2>
+                <p>Módulo en construcción.</p>
+            </div>
+        `;
+    }
+}
